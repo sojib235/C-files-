55398a1cd26e3b74ddf9ed15263e1b9c1933ca27
Sojib Ahmed <s01863356652@gmail.com>
2021-05-23 09:09:30 -0500

    Add files via upload

---
+++ ascii to char.c
@@ -0,0 +1,18 @@
+// how to conver ascii code to letter
+
+
+#include<stdio.h>
+int main(){
+
+
+int i;
+printf("enter any ascii code\n");
+scanf("%d",&i);
+
+printf("the character is %c",i);
+return 0;
+
+
+
+
+}
