209469b4c002a84e4958733daca2d974075dc185
Sojib Ahmed <s01863356652@gmail.com>
2021-05-23 09:10:05 -0500

    Add files via upload

---
+++ uppercase to lower case.c
@@ -0,0 +1,21 @@
+
+//how to covert uppercase letter to lowercase
+
+#include<stdio.h>
+int main()
+{
+
+ char a;
+ printf("Enter your letter\n");
+ scanf("%c",&a);
+ printf("The lowercase character is %c",a+32);
+ return 0;
+
+
+
+
+
+
+
+
+         }
